0f19d6447801e51670756afb67de8e93c01e213a
Aditya Raj <adityaraj34@icloud.com>
2026-04-04 23:50:24 +0530

    booking appoinment

--- src/pages/patitient/Appintements.jsx
+++ src/pages/patitient/Appintements.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Appintements = () => {
+    const {doctorId} =useParams()
   return (
+
     <div>
+        <p>Doctor Id {doctorId}</p>
       
     </div>
   )
