871ef698dd6154e5bbe2a598627a945f3d2e24f9
Aditya Raj <adityaraj34@icloud.com>
2026-04-04 23:39:50 +0530

    route updated

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ const App = () => {
         <Route path='/patient/dashboard' element={<PatientDashboard />} />
         <Route path='/doctor/dashboard' element={<DoctorDashboard />} />
         <Route path='/admin/dashboard' element={<AdminDashboard />} />
+        <Route path='/patient/book/:doctorId' element={<BookAppointment />} />
       </Routes>
     </AuthProvider>
   )
