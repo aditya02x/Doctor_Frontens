20f03a504eb3dbc9fa9e241084bf600e8b773ed0
Aditya Raj <adityaraj34@icloud.com>
2026-04-04 14:00:58 +0530

    Login done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
-import {BrowserRouter , Routes , Route} from "react-router-dom"
-import Signup from './pages/auth/Signup'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { AuthProvider } from './context/AuthContext'
-import {ToastContainer} from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css';
-import Login from '../src/pages/auth/Login'
-import PaitentDashboard  from '../src/pages/patitient/Dashboard'
-import DoctorDashboard from '../src/pages/doctor/Dashboard'
-import AdminDashboard from '../src/pages/admin/Dashboard'
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
+import Login from './pages/auth/Login'
+import Signup from './pages/auth/Signup'
+import PatientDashboard from './pages/patitient/Appintements'
+import DoctorDashboard from './pages/doctor/Dashboard'
+import AdminDashboard from './pages/admin/Dashboard'
 
 const App = () => {
   return (
-  <AuthProvider>
-    <BrowserRouter>
-    <ToastContainer>
-      <Routes>
-        <Route path='/ ' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/Signup' element={<Signup/>} />
-        <Route path='/patient/dashboard' element={<PaitentDashboard/>}/>
-         <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-
-
-
-
-      </Routes>
-    </ToastContainer>
-    </BrowserRouter>
-  </AuthProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <ToastContainer />
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/patient/dashboard' element={<PatientDashboard />} />
+          <Route path='/doctor/dashboard' element={<DoctorDashboard />} />
+          <Route path='/admin/dashboard' element={<AdminDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuth } from '../../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import axios from '../../utils/axios.js'
+import { toast } from 'react-toastify'
+
 
 const Login = () => {
-  return (
-    <div>
+  const [email , setEmail] =useState("")
+  const [password,setPassword] =useState("")
+  const {login} =useAuth()
+  const navigate = useNavigate()
+
+  const handeSubmit = async (e)=>{
+    e.preventDefault()
+    try {
+      const res = await axios.post('/auth/login',{email,password})
+      login(res.data.user , res.data.token)
+      toast.success('Login successful')
+      if(res.data.user.role === 'patient') navigate('/patient/dashboard')
+else if(res.data.user.role === 'doctor') navigate('/doctor/dashboard')
+else if(res.data.user.role === 'admin') navigate('/admin/dashboard')
       
-    </div>
+    } catch (error) {
+       toast.error(error.response.data.message)
+    }
+  }
+
+  return (
+    <section className='h-screen w-full'>
+      <h1 className='text-5xl justify-center items-center'>Login page</h1>
+      <form action="" onSubmit={handeSubmit}>
+        <input type="email " value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter Your Email'/>
+        <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} />
+
+        <button type='submit' >Login</button>
+        
+
+      </form>
+    </section>
   )
 }
 
