61418c118b9ba6ea8818bab1bd97acd78fd560f3
Aditya Raj <adityaraj34@icloud.com>
2026-04-04 12:55:43 +0530

    app.jsx done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter , Routes , Route} from "react-router-dom"
 import Signup from './pages/auth/Signup'
-import Login from './pages/auth/Login'
-
+import { AuthProvider } from './context/AuthContext'
+import {ToastContainer} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css';
+import Login from '../src/pages/auth/Login'
+import PaitentDashboard  from '../src/pages/patitient/Dashboard'
+import DoctorDashboard from '../src/pages/doctor/Dashboard'
+import AdminDashboard from '../src/pages/admin/Dashboard'
 
 const App = () => {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<h1>Welcome to the Home Page</h1>} />
-          <
-       
-      </Router>
-    </div>
+  <AuthProvider>
+    <BrowserRouter>
+    <ToastContainer>
+      <Routes>
+        <Route path='/ ' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/Signup' element={<Signup/>} />
+        <Route path='/patient/dashboard' element={<PaitentDashboard/>}/>
+         <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+
+
+
+      </Routes>
+    </ToastContainer>
+    </BrowserRouter>
+  </AuthProvider>
   )
 }
 
