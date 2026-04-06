67881e3c679094ee8fc94363b7da5e315473e14a
Aditya Raj <adityaraj34@icloud.com>
2026-04-06 23:35:01 +0530

    k

--- src/pages/doctor/Slots.jsx
+++ src/pages/doctor/Slots.jsx
@@ -92,15 +92,11 @@ const Slots = () => {
         />
 
         {/* Button */}
-        <button
-          type='submit'
-          disabled={loading}
-          className='w-full bg-blue-600 text-white py-2 rounded-lg text-sm hover:bg-blue-700 transition disabled:opacity-50'>
-          {loading ? 'Creating...' : 'Create Slot'}
-        </button>
+    
 
       </form>
     </div>
   )
 }
 
+export default Slots
